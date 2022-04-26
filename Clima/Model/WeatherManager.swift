afc67117f71c0d3c27429d7292940d2a0c4cec86
julienshim <25723428+julienshim@users.noreply.github.com>
2022-04-25 19:05:05 -0700

    Weather model

--- Clima/Model/WeatherManager.swift
+++ Clima/Model/WeatherManager.swift
@@ -16,26 +16,17 @@ struct WeatherManager {
     }
     
     func performRequest(urlString: String) {
-        //1. Create a URL
         if let url = URL(string: urlString) {
-            //2. Create a URLSession
             let session = URLSession(configuration: .default)
-            
-            //3. Give the session a task
-//            let task = session.dataTask(with: url, completionHandler: handle(data:response:error:))
             let task = session.dataTask(with: url) { data, response, error in
                 if error != nil {
                     print(error!)
                     return
                 }
                 if let safeData = data {
-//                    let dataString = String(data: safeData, encoding: .utf8)
-//                    print(dataString)
                     self.parseJSON(weatherData: safeData)
                 }
             }
-            
-            //4. Start the task
             task.resume()
         }
     }
@@ -44,13 +35,20 @@ struct WeatherManager {
         let decoder = JSONDecoder()
         do {
             let decodedData = try decoder.decode(WeatherData.self, from: weatherData)
-            print(decodedData.name)
-            print(decodedData.main.temp)
-            print(decodedData.weather[0].description)
+            let id = decodedData.weather[0].id
+            let name = decodedData.name
+            let temp = decodedData.main.temp
+            
+            let weather = WeatherModel(conditionId: id, cityName: name, temperature: temp)
+            
+            print(weather.conditionName)
+            print(weather.temperature)
+            print(weather.temperatureString)
         } catch {
             print(error)
         }
     }
+
     
     //    func handle(data: Data?, response: URLResponse?, error: Error?) {
     //        if error != nil {
